91b954c64a38b0d4e8b0271bf25b5e2daffd4d3d
Naveen Chakka <naveen.chakka@infor.com>
2020-06-07 13:15:44 +0530

    Add project files.

---
+++ SharePointDemo/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using System.IO;
+
+namespace SharePointDemo
+{
+    public class Class1
+    {
+        void Method()
+        {
+
+            var webUrl = "https://yourtenantgoeshere.sharepoint.com/site/yoursitename";
+            var username = "yourusernamegoeshere@yourtenantgoeshere.com";
+            var password = "pleasedonteverwriteyourpasswordincode";
+            var listTitle = "yourdocumentlibrarytitle";
+            var destinationFolder = @"C:temp";
+
+            var securePassword = new SecureString();
+            //Convert string to secure string
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+            securePassword.MakeReadOnly();
+
+            using (var context = new ClientContext(webUrl))
+            {
+                // Connect using credentials -- use the approach that suits you
+                context.Credentials = new SharePointOnlineCredentials(username, securePassword);
+
+                // Get a reference to the SharePoint site
+                var web = context.Web;
+
+                // Get a reference to the document library
+                var list = context.Web.Lists.GetByTitle(listTitle);
+
+                // Get the list of files you want to export. I'm using a query
+                // to find all files where the "Status" column is marked as "Approved"
+                var camlQuery = new CamlQuery
+                {
+                    ViewXml = @"
+            Approved
+            1000
+        "
+                };
+
+                // Retrieve the items matching the query
+                var items = list.GetItems(camlQuery);
+
+                // Make sure to load the File in the context otherwise you won't go far
+                context.Load(items, items2 => items2.IncludeWithDefaultProperties
+                    (item => item.DisplayName, item => item.File));
+
+                // Execute the query and actually populate the results
+                context.ExecuteQuery();
+
+                // Iterate through every file returned and save them
+                foreach (var item in items)
+                {
+                   
+                    using (FileInformation fileInfo = Microsoft.SharePoint.Client.File.OpenBinaryDirect(context, item.File.ServerRelativeUrl))
+                    {
+                        // Combine destination folder with filename -- don't concatenate
+                        // it's just wrong!
+                        var filePath = Path.Combine(destinationFolder, item.File.Name);
+
+                        // Erase existing files, cause that's how I roll
+                        if (System.IO.File.Exists(filePath))
+                        {
+                            System.IO.File.Delete(filePath);
+                        }
+
+                        // Create the file
+                        using (var fileStream = System.IO.File.Create(filePath))
+                        {
+                            fileInfo.Stream.CopyTo(fileStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+   
+}
---
+++ SharePointDemo/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SharePointDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UploadFile();
+
+            //here provide the sitepoint url,path
+            DownloadFilesFromSharePoint("http://sp.frk.com/teams/ResearchSystemsSupport/GT%20Resources/Forms/AllItems.aspx", "/Documents", @"c:\");
+        }
+
+
+        public static void UploadFile()
+        {
+
+
+            String FilePath = @"C:\Project\Sample.txt";
+            string Libirary = "Documents";
+            string SiteUrl = "";
+            string FileName = FilePath.Substring(FilePath.LastIndexOf("\\") + 1);
+
+            using (ClientContext ctx=new ClientContext(SiteUrl))
+            {
+                FileCreationInformation fileCreation = new FileCreationInformation();
+                fileCreation.Url = FileName;
+                fileCreation.Overwrite = true;
+                fileCreation.Content = System.IO.File.ReadAllBytes(FilePath);
+                Web MyLibirary = ctx.Web;
+                List list = MyLibirary.Lists.GetByTitle(Libirary);
+                MyLibirary.RootFolder.Files.Add(fileCreation);
+                ctx.ExecuteQuery();
+            }
+        }
+
+        static void DownloadFilesFromSharePoint(string siteUrl, string siteFolderPath, string localTempLocation)
+        {
+            ClientContext ctx = new ClientContext(siteUrl);
+            ctx.Credentials = new NetworkCredential("username", "password", "Domain");
+
+            FileCollection files = ctx.Web.GetFolderByServerRelativeUrl(siteFolderPath).Files;
+
+            ctx.Load(files);
+            if (ctx.HasPendingRequest)
+            {
+                ctx.ExecuteQuery();
+            }
+
+            foreach (Microsoft.SharePoint.Client.File file in files)
+            {
+                FileInformation fileInfo = Microsoft.SharePoint.Client.File.OpenBinaryDirect(ctx, file.ServerRelativeUrl);
+                ctx.ExecuteQuery();
+
+                var filePath = localTempLocation + "\\" + file.Name;
+                System.IO.FileStream fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite, System.IO.FileShare.ReadWrite);
+
+                fileInfo.Stream.CopyTo(fileStream);
+                
+
+            }
+        }
+ 
+    }
+
+  
+}
